84fa7e01d9ca1601478194d17b9fd5a8615e2c29
Shivani Singh <125179510+Shivanik2002@users.noreply.github.com>
2024-11-06 12:45:28 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3+3)  // code readability should be high
+
+console.log("Shivani")
+
+let name = "shivani"
+let age = 18
+let islength = false
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+// boolean = true/false
+// null => standalone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof null);   // object
+console.log(typeof undefined);  //undefined
+
